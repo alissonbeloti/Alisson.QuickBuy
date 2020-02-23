a7f9a0e9b5daeb655141ded7bf94e0a7f3780e68
alissonbeloti <alisson.beloti@gmail.com>
2020-02-23 13:59:44 -0300

    valid form

--- Alisson.QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ Alisson.QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core'
+import { Usuario } from '../../modelo/usuario';
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.css"]
 })
 export class LoginComponent {
-  entrar() {
+    public usuario
+    
+  constructor() {
+    this.usuario = new Usuario()
+  }
 
+  entrar() {
+    
   }
   onKeypress() {
 
