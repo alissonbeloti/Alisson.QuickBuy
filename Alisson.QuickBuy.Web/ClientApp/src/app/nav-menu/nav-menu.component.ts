f507b6b1d21031642564197a2593b4b6e73c2836
alissonbeloti <alisson.beloti@gmail.com>
2020-02-23 17:45:55 -0300

    autenticação, apenas no lado angular

--- Alisson.QuickBuy.Web/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ Alisson.QuickBuy.Web/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-menu',
@@ -6,6 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-menu.component.css']
 })
 export class NavMenuComponent {
+  constructor(private router: Router) {
+
+  }
   isExpanded = false;
 
   collapse() {
@@ -14,5 +18,17 @@ export class NavMenuComponent {
 
   toggle() {
     this.isExpanded = !this.isExpanded;
+  }
+
+  public usuarioLogado(): boolean {
+    var usuarioLogado = sessionStorage.getItem("usuario-autenticado")
+    if (usuarioLogado == "1") {
+      return true;
+    }
+    return false;
+  }
+  sair() {
+    sessionStorage.setItem("usuario-autenticado", "")
+    this.router.navigate(['/'])
   }
 }
--- Alisson.QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ Alisson.QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,19 +1,27 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { Usuario } from '../../modelo/usuario';
+import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.css"]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
     public usuario
+  public returnUrl = ''
+  constructor(private router: Router, private activatedRouter: ActivatedRoute) {
     
-  constructor() {
-    this.usuario = new Usuario()
   }
+    ngOnInit(): void {
+      this.usuario = new Usuario()
+      this.returnUrl = this.activatedRouter.snapshot.queryParams['returnUrl']
+    }
 
   entrar() {
-    
+    if (this.usuario.email == "alisson@teste.com" && this.usuario.senha == "abc123") {
+      sessionStorage.setItem("usuario-autenticado", "1")
+      this.router.navigate([this.returnUrl])
+    }
   }
   onKeypress() {
 
